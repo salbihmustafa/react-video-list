dfc2fc3757dcca97c0d8b021b31c2f0ae846edf8
salbihmustafa <salbih.mustafa@certegy.com>
2021-11-17 13:28:00 -0500

    add state to searchbar input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React from "react";
 
 class SearchBar extends React.Component {
-    render() {
-        return <div>SearchBar</div>
-    }
+  state = { searchTerm: "" };
+
+  onSearchInputChange = (event) => {
+    this.setState({ searchTerm: event.target.value });
+  };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    // TODO: Make sure we call callback from parent component
+  }
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.searchTerm}
+              onChange={this.onSearchInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
